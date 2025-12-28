9b50c07ea8615ba9d0128fb96affb88095f5d570
TawsifHossain007 <tawsifhossain18.o@gmail.com>
2025-12-28 21:05:03 +0600

    Done

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.port || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./zap-shift-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
 const { stat } = require("fs");
 const { resolveSoa } = require("dns");
 
@@ -423,7 +425,6 @@ async function run() {
           });
         }
 
-        console.log("session retrieve", session);
         const trackingId = generateTrackingId();
 
         if (session.payment_status === "paid") {
@@ -492,10 +493,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
